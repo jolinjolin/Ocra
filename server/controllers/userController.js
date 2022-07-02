4648beeee4ee0be2e6c5efac6ba9d059e467821c
jolin <jolin.xia1@gmail.com>
2022-07-02 18:33:17 -0400

    add user registration

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -36,4 +36,31 @@ const getUserProfile = asyncHandler(async (req, res) => {
     }
 })
 
-export { authUser, getUserProfile }
+//add a user, POST /api/users
+const addUser = asyncHandler(async (req, res) => {
+    const { name, email, password } = req.body
+    const userExist = await User.findOne({ email: email })
+    if (userExist) {
+        res.status(400)
+        throw new Error("User already exist")
+    } else {
+        const user = await User.create({
+            name,
+            email,
+            password
+        })
+        if (user) {
+            res.status(201).json({
+                _id: user._id,
+                name: user.name,
+                email: user.email,
+                isAdmin: user.isAdmin,
+                token: generateToken(user._id)
+            })
+        } else {
+            res.status(400)
+            throw new Error("Invalid user data")
+        }
+    }
+})
+export { authUser, getUserProfile, addUser }
