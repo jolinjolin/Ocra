a59c964e539e5d5a518ac0741ad5c338c220f73e
jolin <jolin.xia1@gmail.com>
2022-07-09 23:45:30 -0400

    prepare for deployment

--- server/app.js
+++ server/app.js
@@ -15,10 +15,6 @@ app.use(express.json())
 
 connectDB()
 
-app.get('/', (req, res) => {
-    res.send('API is running..')
-})
-
 app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
 app.use('/api/orders', orderRoutes)
@@ -30,6 +26,16 @@ app.get('/api/config/paypal', (req, res) => {
 
 const __dirname = path.resolve()
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "/app/build")))
+    app.get("*", (req, res) => res.sendFile(path.resolve(__dirname, "app", 'build', "index.html"))
+    )
+} else {
+    app.get('/', (req, res) => {
+        res.send('API is running..')
+    })
+}
 
 app.use(notFound)
 app.use(errorHandler)
